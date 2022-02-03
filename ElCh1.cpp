538f26c75f7edbcb5a71550dd12252d88fd266b0
Blue <leanefelis@gmail.com>
2022-02-03 14:54:31 +0100

    maj

--- ElCh1.cpp
+++ ElCh1.cpp
@@ -3,12 +3,25 @@ using namespace std; // std is a standard class in C++
 
 class ElCh1
 {
-private: // Privé pour protéger et donner accès grace aux fonctions d'accès (getters / setters en public)
+private:
+    // Privé pour protéger (surtout de la modif des utilisateurs par ex)
+    // + donner accès grace aux fonctions d'accès (getters / setters en public)
     double donnee;
     ElCh1 *suiv;
 
 public:
+    ElCh1(/* args = attributs */);
+    ~ElCh1(); // destructeur (free)
 };
+
+ElCh1::ElCh1(/* args */) // nom de classe ":: = méthode de cette classe"
+{
+    // constructeur = initialiser les données
+}
+
+ElCh1::~ElCh1()
+{
+}
 
 // Main
 int main(int argc, char const *argv[])
