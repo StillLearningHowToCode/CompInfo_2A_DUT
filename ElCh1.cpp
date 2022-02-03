070115041632d51a19074dc041f177e7dd2df9e3
Blue <leanefelis@gmail.com>
2022-02-03 16:50:16 +0100

    ok

--- ElCh1.cpp
+++ ElCh1.cpp
@@ -11,6 +11,7 @@ private:
 
 public:
     ElCh1(double);
+    ElCh1(double, ElCh1);
     ~ElCh1(); // destructeur (free)
 
     // Setter
@@ -19,24 +20,37 @@ public:
         donnee = d;
     }
 
-    void setElCh1()
+    void setElCh1(ElCh1 *suiv)
     {
         this->suiv = suiv;
     }
-    
+
     // Getter
     int getDonnee()
     {
         return donnee;
     }
-};
 
-// constructeur
-ElCh1::ElCh1(double donnee) // nom de classe ":: = méthode de cette classe"
-{
-    // constructeur = initialiser les données
-    this->donnee = donnee;
-}
+    // constructeur
+    ElCh1::ElCh1(double donnee) // nom de classe ":: = méthode de cette classe"
+    {
+        // constructeur = initialiser les données
+        this->donnee = donnee;
+    }
+
+    ElCh1(double newDonnee, ElCh1 *newSuiv)
+    {
+        donnee = newDonnee;
+        suiv = newSuiv;
+    }
+
+    // Fonction afficher
+    void afficher()
+    {
+        // std::string afficher = "[" + parseInt(donnee) + "]"; // création de la chaîne
+        std::cout << "[" << donnee << "]" << '\n'; // affichage de la chaine : << = + (concaténation)
+    }
+};
 
 // Destructeur :
 // un destructeur porte le même nom que la classe dans laquelle il est défini et est précédé d'un tilde
@@ -52,6 +66,10 @@ int main(int argc, char const *argv[])
 {
     std::string message = "Le programme a tourne !"; // création de la chaîne
     std::cout << message << '\n';                    // affichage de la chaine
-    ElCh1 *suiv = new ElCh1(1.62);                   // en "dynamique"
+
+    ElCh1 *suiv = new ElCh1(1.62, NULL); // en "dynamique"
+    ElCh1 *suiv2 = new ElCh1(5.890, suiv);
+    suiv->afficher();
+    suiv2->afficher();
     return 0;
 }
