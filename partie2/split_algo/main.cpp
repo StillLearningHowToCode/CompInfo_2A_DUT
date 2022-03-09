fec8f1c66f394eb2c139fc23a0cc6e1841923e29
Blue <leanefelis@gmail.com>
2022-03-09 17:28:31 +0100

    remplacement

--- partie2/split_algo/main.cpp
+++ partie2/split_algo/main.cpp
@@ -1,27 +1,20 @@
 // C++ program to print words in a sentence
 #include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+using std::cout; using std::cin;
+using std::endl; using std::string;
 using namespace std;
 
 void removeDupWord(string str)
 {
     string word = "";
-    const char *y = "";
     for (auto x : str)
     {
         if (x == ' ')
         {
             cout << word << endl;
             word = "";
-        }
-        else if (y == "si")
-        {
-            cout << word << endl;
-            word = "if";
-        }
-        else if (y == "alors")
-        {
-            cout << word << endl;
-            word = "then";
         }
         else
         {
@@ -36,5 +29,22 @@ int main()
 {
     string str = "si condition alors condition fi";
     removeDupWord(str);
+
+    string input = "si condition alors condition fi";
+    string si = "if";
+    string alors = "then";
+    string fi = "";
+
+    cout << input << endl;
+
+    input.replace(input.find("si"), 2, si);
+    input.replace(input.find("alors"), 5, alors);
+    input.replace(input.find("fi"), 2, fi);
+    removeDupWord(input);
+
+    cout << input << endl;
+
+    return EXIT_SUCCESS;
+
     return 0;
 }
